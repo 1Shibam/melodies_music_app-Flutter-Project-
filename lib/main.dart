ac9c842a626130141de5c6ef4c68d26691df3512
1Shibam <hehe.shibam@gmail.com>
2025-01-08 15:20:01 +0530

    storage permission bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:melodies_music_app/screens/splash_screen.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  requestPermissions();
   runApp(const ProviderScope(child: MelodiesMusicApp()));
+}
+
+Future<void> requestPermissions() async {
+  // Request storage permission on app startup
+  PermissionStatus status = await Permission.storage.request();
+  if (status.isGranted) {
+    print('Permission granted');
+  } else {
+    print('Permission denied');
+  }
 }
 
 class MelodiesMusicApp extends StatelessWidget {
